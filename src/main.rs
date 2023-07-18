19d50180e2a010675c9338365be9d316e0b863b3
Blake Costa <blakecosta117@gmail.com>
2023-07-18 12:22:26 -0400

    Working bevy example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use rand::Rng;
+use std::f32::consts::PI;
 use std::thread::spawn;
 
 // TODO - Remove or refactor the use of these variables
@@ -9,11 +11,97 @@ pub const HEIGHT: f32 = 1080.0;
 pub const WIDTH: f32 = 1920.0;
 
 // new tower component with a Timer
-#[derive(Component)]
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
 pub struct Tower {
     shooting_timer: Timer,
 }
 
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Lifetime {
+    timer: Timer,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Target {
+    speed: f32,
+    direction: Vec3,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Health {
+    value: i32,
+}
+
+// fn move_targets(
+//     mut commands: Commands,
+//     mut targets: Query<(Entity, &mut Target)>, // Entity is only one don't need & or &mut
+//     time: Res<Time>,
+// ) {
+//     // iterate over Lifetimes on bulet component
+//     // only gets entities with Lifetime component
+//     for (entity, mut target) in &mut targets {
+//         //target.transform.translation += target.direction * time.delta_seconds();
+
+//         // check if timer just finished and despawn bullet
+//         if lifetime.timer.just_finished() {
+//             commands.entity(entity).despawn_recursive(); // almost always use despawn_recursive
+//         }
+//     }
+// }
+fn tower_shooting(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut towers: Query<&mut Tower>, // gets all tower components in game. can only use &mut or & for Query
+    time: Res<Time>,
+) {
+    // get a random number between 0.0 and 4.0 for some fun
+    let mut rng = rand::thread_rng();
+    let random_float: f32 = rng.gen_range(0.0..4.0);
+    // iterate over tower components queried
+    for mut tower in &mut towers {
+        // tick the timer
+        tower.shooting_timer.tick(time.delta());
+        // check if timer just finished and spawn bullet
+        if tower.shooting_timer.just_finished() {
+            // create new transform for bullet
+            let spawn_transform = Transform::from_xyz(0.0, 0.7, random_float)
+                .with_rotation(Quat::from_rotation_y(-PI / 2.0));
+            // pbr bundle - bullet
+            commands
+                .spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
+                    material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                    transform: spawn_transform,
+                    ..default()
+                })
+                .insert(Lifetime {
+                    // add a lifetime component to the bullet
+                    timer: Timer::from_seconds(0.5, TimerMode::Once),
+                })
+                .insert(Name::new("Bullet"));
+        }
+    }
+}
+fn bullet_despawn(
+    mut commands: Commands,
+    mut bullets: Query<(Entity, &mut Lifetime)>,
+    time: Res<Time>,
+) {
+    // iterate over Lifetimes on bulet component
+    // only gets entities with Lifetime component
+    for (entity, mut lifetime) in &mut bullets {
+        lifetime.timer.tick(time.delta());
+        // check if timer just finished and despawn bullet
+        if lifetime.timer.just_finished() {
+            commands.entity(entity).despawn_recursive(); // almost always use despawn_recursive
+        }
+    }
+}
 fn spawn_camera(mut commands: Commands) {
     // camera
     commands.spawn(Camera3dBundle {
@@ -50,6 +138,23 @@ fn spawn_basic_scene(
             shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
         }) // insert tower component
         .insert(Name::new("Tower")); //entity name
+
+    // basic shape moving
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube {
+                size: 0.1,
+                ..Default::default()
+            })),
+            material: materials.add(Color::rgb(0.5, 0.7, 0.4).into()),
+            transform: Transform::from_xyz(0.0, 1.0, 0.0),
+            ..default()
+        })
+        .insert(Target {
+            speed: 4.0,
+            direction: Vec3::Y,
+        })
+        .insert(Name::new("Moving Target")); //entity name
 }
 fn spawn_light(mut commands: Commands) {
     // light
@@ -72,8 +177,14 @@ fn main() {
         .add_startup_system(spawn_basic_scene) // create a basic scene
         .add_startup_system(spawn_camera) // spawn a camera
         .add_startup_system(spawn_light) // spawn a light
+        .register_type::<Tower>() // Register Custom Tower component
+        .register_type::<Lifetime>() // Register Lifetime component
+        .register_type::<Target>() // Register Target component
+        .register_type::<Health>() // Register Health component
         .add_plugins(DefaultPlugins) // add default bevy plugins
         .add_plugin(WorldInspectorPlugin::new()) // add the plugin for setting up inspector
         .add_plugin(FrameTimeDiagnosticsPlugin::default()) // add the plugin for displaying fps
+        .add_system(tower_shooting)
+        .add_system(bullet_despawn) // despawn bullet
         .run();
 }
